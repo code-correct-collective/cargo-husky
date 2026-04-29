7cab6934a78c9d144f3bd00560404ef7a6673cac
ghorsey <geoff.horsey@outlook.com>
2026-04-28 17:59:50 -0700

    feat: working on starting to add git repository

--- src/husky.rs
+++ src/husky.rs
@@ -1,3 +1,4 @@
+use git2::{Repository, RepositoryOpenFlags};
 use rust_embed::Embed;
 
 #[derive(Embed)]
@@ -6,6 +7,7 @@ struct Assets;
 
 pub fn install(directory: &str) {
     let hook = Assets::get("hook").unwrap();
+    let repository = Repository::open_ext(".", RepositoryOpenFlags::empty(), &[] as &[&std::ffi::OsStr]);
 
     println!("Add script to {:?} to {directory}", std::str::from_utf8(hook.data.as_ref()).unwrap());
     todo!("create the function to initialize github");
