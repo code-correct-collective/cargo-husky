589184ff3e7fd61c52fe2ea2564091d467109372
ghorsey <geoff.horsey@outlook.com>
2026-05-07 22:04:19 -0700

    feat: adding rc types

--- src/husky/tests.rs
+++ src/husky/tests.rs
@@ -1,4 +1,15 @@
+use std::path::PathBuf;
+
+use crate::husky::repository::{HuskyRepository, MockHuskyRepository};
+
 #[test]
-pub fn silly_test() {
-    assert!(true == true)
+pub fn list_task_actions() {
+    // arrange
+    let mut mock_repo = MockHuskyRepository::new();
+    mock_repo
+        .expect_get_husky_path()
+        .returning(|| Ok(PathBuf::from(".husky/task_runner.json")));
+
+    let r = mock_repo.get_husky_path().unwrap();
+    assert_eq!(r, PathBuf::from(".husky"))
 }
