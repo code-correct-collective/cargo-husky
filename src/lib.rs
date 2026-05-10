0325027384e69d25dad0bc77de18009f5daa3a25
Geoff <geoff.horsey@outlook.com>
2026-05-10 10:38:17 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! The core library of the application to create git hooks and run tasks/commands when those hook scripts execute.
+//! The core library crate of the application to create git hooks and run tasks/commands when those hook scripts execute.
 
 /// The cli arguments used by the executable
 pub mod cli;
